0b48725ffd2cf16fb99e017115b9afc2a060deff
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-10-13 14:35:21 +0300

    feat: add map

--- museum-dom/src/index.js
+++ museum-dom/src/index.js
@@ -3,15 +3,20 @@ import './scss/style.scss'
 import './js/video.js';
 import './js/menu.js';
 import './js/script.js';
-import './js/player.js';
-import './js/swiper.js';
+//import './js/player.js';
+//import './js/swiper.js';
 //import './js/scroll.js';
 //import Swiper from './js/swiper-bundle.min.js';
 //import playVideo from './js/player.js';
 //import galleryFn from  './js/gallery.js';
 import loadImages from './js/gallery.js';
+import playVideo from './js/player.js';
 import initComparisons from './js/explore.js';
 import animOnScroll from './js/scroll.js';
+import calcPrice from './js/tickets.js';
+import formValidation from './js/form.js';
+
+
 
 import Swiper, { Navigation, Pagination } from 'swiper';
   // import Swiper and modules styles
@@ -89,118 +94,41 @@ const mainSliderCountSlides = document.querySelector('.main-slider__total');
 const mainSliderCurrentSlides = document.querySelector('.main-slider__current');
 
 mainSliderCountSlides.innerHTML = mainSlider.slides.length - 2;
-console.log(mainSlider.slides.length);
 mainSlider.on('slideChange', function() {
     let currentSlide = ++mainSlider.realIndex;
    
     mainSliderCurrentSlides.innerHTML = currentSlide;
 });
 
-const videoMiniSlider = new Swiper('.video-slider', {
-  // Optional parameters
-    
-  
-  
-  // If we need pagination
-  pagination: {
-    el: '.control__dots',
-    //Буллеты
-    
-    type: 'bullets',
-    clickable: true,
-    dynamicBullets: true,
-  },
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxlbmFwb2dvZGFldmEiLCJhIjoiY2t1b2Nrazk0Mjl6NDJxbjZ5dnQxNXN0NiJ9.dLhBNeoljyMUjwMl47Pj4A';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/light-v10',
+center: [2.3364, 48.86091],
+zoom: 16
+});
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.video-slider .control__arrow-next',
-    prevEl: '.video-slider .control__arrow-prev', 
-  /*  type: 'fraction',
-    renderFraction: function(currentClass, totalClass) {
-      return '<span class = "'+ currentClass + '"></span>' +
-      '<span class = "'+ totalClass + '"></span>'
-    } */
-  },
-simulateTouch: true,
-//slideToClickedSlide: true,
- // autoHeight: true,
-  slidesPerView:3,
-  watchOverflow: true,
-  spaceBetween: 42,
-  loop: true, //бесконечный слайдер
- // slidesPerGroup:3,
-  // And if we need scrollbar
- // freeMode: true, //свободный режим
- // Автопрокрутка 
- freeMode: true,
- loopedSlides: 5, //looped slides should be the same
- watchSlidesProgress: true,
- speed: 300,
- autoplay: {
-   delay:1000,
-   stopOnLastSlide: true,
-   disableOnInteraction: false,
- },
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker({color: '#868080'})
+.setLngLat([2.3333, 48.8602])
+.addTo(map);
  
+// Create a default Marker, colored black, rotated 45 degrees.
+const marker2 = new mapboxgl.Marker({color: '#868080'})
+.setLngLat([2.3397, 48.8607])
+.addTo(map);
 
-});
+const marker3 = new mapboxgl.Marker({color: '#868080'})
+.setLngLat([2.3330, 48.8619])
+.addTo(map);
 
-const videoSlider = new Swiper('.video-test', {
-  // Optional parameters
-    
-  
-  
-  // If we need pagination
-  
+const marker4 = new mapboxgl.Marker({color: '#868080'})
+.setLngLat([2.3365, 48.8625])
+.addTo(map);
 
-  // Navigation arrows
-  
-simulateTouch: true,
-//slideToClickedSlide: true,
- // autoHeight: true,
-  slidesPerView:1,
-  watchOverflow: true,
-  //spaceBetween: 30,
-  loop: true, //бесконечный слайдер
- // slidesPerGroup:3,
-  // And if we need scrollbar
- // freeMode: true, //свободный режим
- // Автопрокрутка 
- loopedSlides: 5, //looped slides should be the same
- speed: 300,
- thumbs: {
-  swiper: {
-   videoMiniSlider,
-    
-  }
- },
- 
-
-});
-
-
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  loop: true,
-  freeMode: true,
-  loopedSlides: 5, //looped slides should be the same
-  watchSlidesProgress: true,
-});
-var galleryTop = new Swiper('.gallery-top', {
-  spaceBetween: 10,
-  loop: true,
-  loopedSlides: 5, //looped slides should be the same
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  thumbs: {
-    swiper: galleryThumbs,
-  },
-});
-
-
+const marker5 = new mapboxgl.Marker({color: '#000000'})
+.setLngLat([2.3364, 48.86091])
+.addTo(map);
 
 //window.onload = loadImages;
 window.onload = function() {
@@ -208,5 +136,8 @@ window.onload = function() {
   initComparisons();
   
 }
+playVideo();
 animOnScroll();
+calcPrice();
+formValidation();
 //window.onload = initComparisons;
