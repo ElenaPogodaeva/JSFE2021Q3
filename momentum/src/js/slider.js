f58df065bc8acaeaf755473e725c571cab427d88
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-10-25 11:39:17 +0300

    feat: get image from Unsplash API

--- momentum/src/js/slider.js
+++ momentum/src/js/slider.js
@@ -2,6 +2,15 @@ export default function changeBg() {
   const body = document.querySelector("body");
   const slideNext = document.querySelector(".slide-next");
   const slidePrev = document.querySelector(".slide-prev");
+
+
+  const photoSrc  = document.querySelectorAll('input[name=photo-source]');
+  let photo = "GitHub";
+
+  const inputTags = document.querySelector(".settings-input");
+  let tags = ''; 
+
+
   let randomNum;
 
   function getRandomNum() {
@@ -15,25 +24,43 @@ export default function changeBg() {
     const timesOfDay = ["night", "morning", "afternoon", "evening"];
     return timesOfDay[Math.floor(hours / 6)];
   }
-  function setBg() {
+
+
+  function setBg(src) {
+    const img = new Image();
+    img.src = src;
+    //const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+    img.onload = () => {
+      body.style.backgroundImage = `url(${img.src})`;
+    };
+  }
+
+  function getGutHubLink() {
     const timeOfDay = getTimeOfDay();
     //const num = getRandomNum().toString();
     const bgNum = randomNum.toString().padStart(2, "0");
     // const bgNum = '15'.padStart(2, '0');
 
-    const img = new Image();
+   // const img = new Image();
     // const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
 
-    img.src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
-
+    const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    setBg(src);
     //const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
-    img.onload = () => {
-      body.style.backgroundImage = `url(${img.src})`;
-    };
+   // img.onload = () => {
+   //   body.style.backgroundImage = `url(${img.src})`;
+   // };
 
     console.log(randomNum);
-    console.log(img.src);
+   // console.log(img.src);
   }
+
+
+  
+
+  
+
+
 
   function getSlideNext() {
     if (randomNum < 20) {
@@ -41,7 +68,7 @@ export default function changeBg() {
     } else {
       randomNum = 1;
     }
-    setBg();
+    getGutHubLink();
   }
 
   function getSlidePrev() {
@@ -50,10 +77,68 @@ export default function changeBg() {
     } else {
       randomNum = 20;
     }
-    setBg();
+    getGutHubLink();
   }
-  window.addEventListener("load", setBg);
-  slideNext.addEventListener("click", getSlideNext);
-  slidePrev.addEventListener("click", getSlidePrev);
+
+
+  async function getLinkToImage(tags) {
+    if (tags === '') {
+      tags = getTimeOfDay();
+      
+    }
+   // const timeOfDay = getTimeOfDay();
+    const url = `https://api.unsplash.com/photos/random?query=${tags}&client_id=oVE-N1MJGgo0-9JrJ21Qdv0m__vGtwbV0OvlfUutJMg`;
+    const res = await fetch(url); //${timeOfDay}
+    const data = await res.json();
+
+console.log(url);
+console.log(tag);
+    // const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+
+    const src = data.urls.regular;
+    //const src = `https://raw.githubusercontent.com/ElenaPogodaeva/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+    setBg(src);
+
+   // body.style.backgroundImage = `url(${data.urls.regular})`;
+    console.log(data.urls.regular);
+}
+
+function showNextSlide() {
+  if (photo === 'GitHub') {
+    getSlideNext();
+  }
+  else if (photo === 'Unsplash API') {
+    getLinkToImage(tags);
+  }
+}
+function showPrevSlide() {
+  if (photo === 'GitHub') {
+    getSlidePrev();
+  }
+  else if (photo === 'Unsplash API') {
+    getLinkToImage(tags);
+  }
+}
+
+function setSrcValue() {
+  const photoSrcChecked = document.querySelector('input[name=photo-source]:checked');
+   photo = photoSrcChecked.value;  
+   console.log(photo);
+   
+}
+
+
+  window.addEventListener("load", getGutHubLink);
+
+  photoSrc.forEach(el => el.addEventListener('click', setSrcValue));
+
+
+  inputTags.addEventListener("change", () => {
+    tags = inputTags.value;
+    console.log(tags);
+  });
+
+  slideNext.addEventListener("click", showNextSlide);
+  slidePrev.addEventListener("click", showPrevSlide);
   
 }
