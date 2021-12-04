efec6d3e3da2ecb7a96f78119ef8306f19d9ae0b
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-12-04 23:06:35 +0300

    feat: create interfaces for data exchange with News API

---
+++ migration-newip-to-ts/src/components/types.ts
@@ -0,0 +1,20 @@
+export interface IDataSource {
+  id: string | null;
+  name: string;
+}
+
+export interface IDataArticle {
+  source: IDataSource;
+  author: string | null;
+  title: string;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+}
+
+export interface IData {
+  articles?: Array<IDataArticle>;
+  sources?: Array<IDataSource>;
+}
