a207cfeffa9bb954208034adee42e841ef61bea8
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-12-04 22:53:02 +0300

    feat: rename *.js files to *.ts files

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '8fefef8c02fb4deaab9897adb49f47a0', // получите свой ключ https://newsapi.org/
         });
     }
 }
--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -13,7 +13,7 @@ const baseConfig = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.[tj]s$/i,
+                test: /\.ts$/i,
                 use: 'ts-loader',
                 exclude: /node-modules/,
             },
