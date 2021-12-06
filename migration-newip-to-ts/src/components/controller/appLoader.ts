80b57d02bfe6e9f66a1d4728aba98635174611c7
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-12-06 23:36:29 +0300

    fix: fix url

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
-      apiKey: '8fefef8c02fb4deaab9897adb49f47a0', // получите свой ключ https://newsapi.org/
+    super('https://nodenews.herokuapp.com/', {
+      apiKey: 'bebcc0c0bf374412acfa48fb911daffd', // получите свой ключ https://newsapi.org/
     });
   }
 }
