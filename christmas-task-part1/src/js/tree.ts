69869ec4aa6338925230f29251ff2c2eff4ea362
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-12-27 20:27:19 +0300

    feat: add garland

--- christmas-task-part1/src/js/tree.ts
+++ christmas-task-part1/src/js/tree.ts
@@ -36,3 +36,90 @@ function setBg(e: Event): void {
 
 bgContainer.addEventListener('click', (e: Event) => setBg(e));
 
+
+function addGarland(): void {
+  var startAngle = Math.PI / 10,
+  angle = startAngle / 2,
+  radius = 100,
+  offset = 500 / 2;
+ /* addGarlandRow(480, 15, 10, 20);
+  addGarlandRow(460, 35, 20, 20);
+  addGarlandRow(435, 55, 30, 20);
+  addGarlandRow(400, 90, 40, 20);*/
+  addGarlandRow(480, 15, 8, 20);
+  addGarlandRow(450, 45, 18, 20);
+  addGarlandRow(400, 95, 28, 20);
+  addGarlandRow(380, 115, 38, 20);
+  addGarlandRow(320, 175, 48, 20);
+  addGarlandRow(280, 215, 58, 20);
+
+}
+//addGarland();
+
+function addGarlandRow(right:number, left:number, bottom:number, length:number): void {
+  var startAngle = Math.PI / 10,
+  //angle = startAngle / 2,
+  radius = 100,
+  offset = 500 / 2;
+
+  const ul = document.createElement('ul');
+  ul.classList.add('lightrope');
+  let width = (right - left)*2;
+  let count = width/30;
+  let angle = 60 / count;
+  let rotate = 60;
+  ul.style.width = `${width}px`;
+  ul.style.height = `${width}px`;
+  ul.style.bottom = `${bottom}%`;//'555px';
+  garlandContainer.appendChild(ul);
+  
+  
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement('li');
+   // li.style.left = radius * Math.cos( angle ) + offset + "px";
+   // li.style.top = radius * Math.sin( angle ) + "px";
+     li.classList.add(`${garlandColor}`);
+  //  angle += startAngle;
+   
+    ul.appendChild(li);
+    
+   // li.style.transform = `translate(${x}px)`;
+    li.style.transform = `rotate(${rotate}deg) translate(${Math.round(width/2 - 10)}px) rotate(-${rotate}deg)`;
+   // x +=30;
+   //rotate+=4;
+   rotate = rotate + angle;
+  }
+}
+
+function changeGarlandColor(): void {
+  isGarlandOn = true;
+  garlandContainer.innerHTML = '';
+  switchGarlandCheckbox.checked=true;
+
+  const colorChecked = document.querySelector('.garland-radio:checked') as HTMLInputElement;
+  //if ((e.target as HTMLElement).classList.contains('garland-btn')) {
+    console.log(colorChecked);
+   // color = (e.target as HTMLElement).dataset.color as string;
+   garlandColor = colorChecked.value;
+    console.log(garlandColor);
+  //  mainTree.src=`./assets/tree/${treeNum}.png`;
+  addGarland();
+    
+ // }
+
+}
+
+garlandBtns.forEach(item => item.addEventListener('click',changeGarlandColor));
+
+function removeGarland(): void {
+  garlandContainer.innerHTML = '';
+  //isGarlandOn = false;
+  switchGarlandCheckbox.checked=false;
+}
+
+function switchGarland(): void {
+  isGarlandOn = switchGarlandCheckbox.checked;
+  isGarlandOn ? addGarland() : removeGarland();
+}
+
+switchGarlandCheckbox.addEventListener('change', switchGarland);
