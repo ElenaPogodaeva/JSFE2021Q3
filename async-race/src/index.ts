c0d9c33256a2999ab9312778aa726e4a0e827443
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-01-19 21:43:52 +0300

    feat: add pagination

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,3 +1,42 @@
+import {render} from './pages/garage/garage';
+import { getCars, getCar, createCar, updateCar, deleteCar, updateGarage} from './api'
+
+import { renderGarage} from './pages/garage/garage';
+import store from './store';
+
+
+render();
+await updateGarage();
+
+
+const nextBtn = document.getElementById('next-btn') as HTMLButtonElement;
+const prevBtn = document.getElementById('prev-btn') as HTMLButtonElement;
+
+const nextBtnClick = async () => {
+  if (store.view === 'garage') {
+    store.carsPage += 1;
+    await updateGarage();
+    const garage = document.getElementById('garage') as HTMLElement;
+
+    garage.innerHTML = renderGarage();
+  }
+}
+
+const prevBtnClick = async () => {
+  if (store.view === 'garage') {
+    store.carsPage -= 1;
+    await updateGarage();
+    const garage = document.getElementById('garage') as HTMLElement;
+
+    garage.innerHTML = renderGarage();
+  }
+}
+nextBtn.addEventListener('click', nextBtnClick);
+prevBtn.addEventListener('click', prevBtnClick);
+
+
+
+  //console.log(createForm);
 /*import image from './images/lazy.png';
 
 const createImage = (src: string) => new Promise<HTMLImageElement>((res, rej) => {
