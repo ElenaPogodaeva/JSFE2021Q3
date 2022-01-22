5d819f42c97b22d2ed4f0e16f68f793659c77ec2
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-01-22 17:44:43 +0300

    feat: add class Car

---
+++ async-race/src/model/car.model.ts
@@ -0,0 +1,15 @@
+interface ICar {
+  name: string;
+  color: string;
+  id: number;
+}
+export default class Car implements ICar {
+  id: number;
+  name: string;
+  color: string;
+  constructor(id: number, name: string, color: string) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+  }
+}
